22f4bbe9c781ce5280a5515f2ff151841fd086fa
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-20 08:33:23 -0500

    sudo for backups

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -20,4 +20,20 @@
       monthly = 12;
     };
   };
+
+  # This is required to run the backup scripts without entering a password
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        groups = [ "wheel" ];
+        commands = [
+          {
+            command = "ALL";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
+  };
 }
